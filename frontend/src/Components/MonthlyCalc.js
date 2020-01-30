8f53859591c27bb27157237d450e0e3c7e24abe6
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-01-30 15:13:58 -0600

    added a bit of styling just so I could work on function easier

--- frontend/src/Components/MonthlyCalc.js
+++ frontend/src/Components/MonthlyCalc.js
@@ -1,5 +1,32 @@
 import React, { useState, useEffect } from "react";
 import * as Yup from "yup";
+import styled from "styled-components";
+
+const Month = styled.div`
+  display: flex;
+  margin: 2%;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Calc = styled.form`
+  display: flex;
+  width: 25%;
+  flex-direction: column;
+  margin: 3%;
+  padding: 2%;
+`;
+
+const Label = styled.label`
+  display: flex;
+  margin: 2%;
+  width: 100%;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+`;
 
 const MonthlyCalc = ({ errors, touched, values, status }) => {
   const [monthlyBudget, setMonthlyBudget] = useState("");
@@ -11,40 +38,52 @@ const MonthlyCalc = ({ errors, touched, values, status }) => {
   return (
     <div className="monthlyForm">
       <h1>Monthly Expenditures</h1>
-      <form>
-        <label>Transportation Expense?</label>
+
+      <Calc>
+        <Month>
+          <label>Monthly Income</label>
+          <input
+            type="text"
+            name="monthlyIncome"
+            placeholder="Monthly Income"
+          ></input>
+        </Month>
+        <Label>Transportation Expense?</Label>
         <input
           type="text"
           name="transportation"
           placeholder="Transportation"
         ></input>
-        <label>Food Expense?</label>
+        <Label>Food Expense?</Label>
         <input type="text" name="food" placeholder="Cost of Food"></input>
-        <label>Health Insurance?</label>
+        <Label>Health Insurance?</Label>
         <input
           type="text"
           name="healthInsurance"
           placeholder="Health Insurance Cost"
         ></input>
-        <label>Car Insurance?</label>
+        <Label>Car Insurance?</Label>
         <input
           type="text"
           name="carInsurance"
           placeholder="Car Insurance Cost"
         ></input>
-        <label>Car Note Cost?</label>
+        <Label>Car Note Cost?</Label>
         <input type="text" name="carNote" placeholder="Car Note Cost"></input>
-        <label>Personal Loans?</label>
+        <Label>Personal Loans?</Label>
         <input
           type="text"
           name="personalLoans"
           placeholder="Personal Loans"
         ></input>
-        <label>Other Costs</label>
+        <Label>Other Costs</Label>
         <input type="text" name="otherCosts" placeholder="Other"></input>
-      </form>
+      </Calc>
+      <button type="submit">Calculate</button>
     </div>
   );
+
+  monthlyBudget.map(MonthlyBudget => {});
 };
 
 export default MonthlyCalc;
