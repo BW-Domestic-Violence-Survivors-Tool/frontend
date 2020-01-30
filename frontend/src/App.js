903caaca7970c593823ede985fa3767eebf04994
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-01-29 21:08:55 -0600

    added the file structure of the minimal amount, also added the first form components

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import MonthlyCalc from "./Components/MonthlyCalc";
+
+function App() {
+  return <MonthlyCalc />;
+}
+
+export default App;
---
+++ frontend/src/Components/MonthlyCalc.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+
+const MonthlyCalc = ({ errors, touched, values, status }) => {
+  const [monthlyBudget, setMonthlyBudget] = useState("");
+
+  useEffect(() => {
+    setMonthlyBudget(Budget => [...monthlyBudget, status]);
+  }, [status]);
+
+  return (
+    <div className="monthlyForm">
+      <h1>Monthly Expenditures</h1>
+      <form>
+        <label>Transportation Expense?</label>
+        <input
+          type="text"
+          name="transportation"
+          placeholder="Transportation"
+        ></input>
+        <label>Food Expense?</label>
+        <input type="text" name="food" placeholder="Cost of Food"></input>
+        <label>Health Insurance?</label>
+        <input
+          type="text"
+          name="healthInsurance"
+          placeholder="Health Insurance Cost"
+        ></input>
+        <label>Car Insurance?</label>
+        <input
+          type="text"
+          name="carInsurance"
+          placeholder="Car Insurance Cost"
+        ></input>
+        <label>Car Note Cost?</label>
+        <input type="text" name="carNote" placeholder="Car Note Cost"></input>
+        <label>Personal Loans?</label>
+        <input
+          type="text"
+          name="personalLoans"
+          placeholder="Personal Loans"
+        ></input>
+      </form>
+    </div>
+  );
+};
+
+export default MonthlyCalc;
